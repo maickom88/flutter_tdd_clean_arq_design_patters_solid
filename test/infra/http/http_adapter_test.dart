0d6ab7236a943ef4e8a41cef09ca09b5104fa0f5
Michael Frank <maickomtutos@gmail.com>
2020-09-14 14:57:47 -0300

    ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -20,7 +20,12 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
-    await client.post(url, headers: headers, body: json.encode(body));
+    final jsonBody = body != null ? json.encode(body) : null;
+    await client.post(
+      url,
+      headers: headers,
+      body: jsonBody,
+    );
   }
 }
 
@@ -47,6 +52,18 @@ void main() {
           'accept': 'application/json',
         },
         body: '{"any_key":"any_value"}',
+      ));
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      verify(client.post(
+        any,
+        headers: anyNamed('headers'),
       ));
     });
   });
