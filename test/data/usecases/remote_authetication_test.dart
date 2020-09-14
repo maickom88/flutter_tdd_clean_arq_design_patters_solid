204f4d851eb0e46523b2c04938796d10028cd8a1
Michael Frank <maickomtutos@gmail.com>
2020-09-14 11:08:08 -0300

    ensure RemouteAuthentication returns an Account on 200

--- test/data/usecases/remote_authetication_test.dart
+++ test/data/usecases/remote_authetication_test.dart
@@ -25,6 +25,17 @@ void main() {
     sut = RemoteAuthentication(httpClient: httpClient, url: url);
   });
   test('Should call HttpClient whit correct values', () async {
+    when(
+      httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body'),
+      ),
+    ).thenAnswer((_) async => {
+          "accessToken": faker.guid.guid(),
+          "name": faker.person.name(),
+        });
+
     await sut.auth(params);
 
     verify(
@@ -89,5 +100,22 @@ void main() {
     final future = sut.auth(params);
 
     expect(future, throwsA(DomainError.invalidCredencials));
+  });
+  test('Should return a Account if HttpClient returns 200', () async {
+    final accessToken = faker.guid.guid();
+    when(
+      httpClient.request(
+        url: anyNamed('url'),
+        method: anyNamed('method'),
+        body: anyNamed('body'),
+      ),
+    ).thenAnswer((_) async => {
+          "accessToken": accessToken,
+          "name": faker.person.name(),
+        });
+
+    final account = await sut.auth(params);
+
+    expect(account.token, accessToken);
   });
 }
